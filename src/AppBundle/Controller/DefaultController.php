325d680324201a5257defa4b745a79ab662af440
Hani Yagoub <hani.yagoub@gmail.com>
2018-01-26 12:36:54 +0100

    added assetic

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -18,4 +18,13 @@ class DefaultController extends Controller
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
     }
+
+    /**
+     * @Route("/video", name="video")
+     */
+    public function videoAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('AppBundle:toto:video.html.twig');
+    }
 }
